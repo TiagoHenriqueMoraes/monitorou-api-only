29b0f77e9758da8fba3db2fd1d83f769bf41c8c3
TiagoMoraes <tiago_h_moraes@hotmail.com>
2018-11-06 20:27:17 -0300

    Refactored on worktimes model

--- db/migrate/20181018001956_create_worktimes.rb
+++ db/migrate/20181018001956_create_worktimes.rb
@@ -2,6 +2,7 @@ class CreateWorktimes < ActiveRecord::Migration[5.2]
   def change
     create_table :worktimes do |t|
       t.datetime :worktime
+      t.integer :day
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_002203) do
 
   create_table "worktimes", force: :cascade do |t|
     t.datetime "worktime"
+    t.integer "day"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
