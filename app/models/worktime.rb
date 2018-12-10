14b88d73cf0ae2c08cc6d723acd90207bd33692f
TiagoMoraes <tiago_h_moraes@hotmail.com>
2018-12-10 21:56:36 -0200

    Fixed worktimes error

--- app/models/worktime.rb
+++ app/models/worktime.rb
@@ -5,13 +5,4 @@ class Worktime < ApplicationRecord
 
   scope :user, -> user { where(user_id: user) }
 
-  def as_json(options={})
-    {       
-      id: self.id,
-      user_id: self.user_id,
-      day: self.day,
-      end_time: self.end_time.strftime("%H:%M:%S"),
-      start_time: self.start_time.strftime("%H:%M:%S")
-    }
-  end
 end
