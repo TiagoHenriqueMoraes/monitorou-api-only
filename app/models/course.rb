baa51a4b3b3c3cb60ddbf084a58bf2080c9a5aea
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2019-03-25 19:51:03 -0300

    Fixed migrations created N:N relation to course and subjects, better returns for main object

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :institution
-  has_many :subjects
+  
   has_many :users
-
+  
+  has_and_belongs_to_many :subjects
+  
   scope :institution, -> institution { where(institution_id: institution) }
   scope :subjects, -> { course.subjects }
 end
