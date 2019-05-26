a866ca336841086994dc6a4912a25c1dff802680
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2019-05-26 15:41:49 -0300

    Added field description to contents, fixed returns for each endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_223113) do
+ActiveRecord::Schema.define(version: 2019_05_26_182043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_223113) do
     t.integer "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["study_group_id"], name: "index_contents_on_study_group_id"
   end
 
